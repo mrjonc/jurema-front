3ed29a175cc480804f6bf3ed5c60ede7e5faad55
mrjonc <jonathanlaureano33.jl@gmail.com>
2026-09-01 18:08:50 -0300

    ajustando o css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,22 +5,20 @@ function Login() {
     <>
       <div className={styles.container}>
         <div className={styles.imgForm}>
-          <img src="./imgtest.png" alt="Imagem do formulario" />
+          <img src="/imgtest.png" alt="Imagem do formulario" />
         </div>
         <div className={styles.form}>
           <h1>Login do Estudante</h1>
           <form action="username" method="post">
             <label htmlFor="username">Email:</label>
             <input type="text" id="username" name="username" required />
-            <br />
-            <br />
+
             <label htmlFor="password">Senha:</label>
             <input type="password" id="password" name="password" required />
-            <br />
-            <br />
+
             <button type="submit">Entrar</button>
           </form>
-          <h2 className="texto-pequeno">
+          <h2 className={styles.textPequeno}>
             Não tem uma conta? <a href="/cadastro">Cadastre-se aqui</a>
           </h2>
         </div>
