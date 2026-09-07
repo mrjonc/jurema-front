f5d8af2237772bfe7eef6ae2ca6bd2b4b27b4a43
mrjonc <jonathanlaureano33.jl@gmail.com>
2026-09-07 17:13:14 -0300

    debugando o codigo do login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    !
     <div className={styles.loginPage}>
       <div className={styles.container}>
         <div className={styles.imgForm}>
@@ -13,26 +12,32 @@ function Login() {
           <h1>Login</h1>
           <form action="username" method="post">
             <div className={styles.inputs}>
-              <input type="text" id="username" name="username" placeholder="E-mail"  required />
-              <input type="password" id="password" name="password" placeholder="Senha" required />
-              <button type="submit"><b>Entrar</b></button>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="E-mail"
+                required
+              />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Senha"
+                required
+              />
+              <button type="submit">
+                <b>Entrar</b>
+              </button>
             </div>
-           
-           
-
-            
           </form>
           <h6 className={styles.textPequeno}>
             Não tem uma conta? <Link to="/register">Cadastre-se aqui</Link>
           </h6>
-          
         </div>
       </div>
     </div>
-     
-    
   );
 }
 
 export default Login;
-
